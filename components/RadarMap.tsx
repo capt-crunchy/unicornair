1b51ddaa68f62b95c9f2ae8bcb5f94f5f4562b8e
capt-crunchy <flip65505@gmail.com>
2025-10-31 11:48:54 -0700

    Update RadarMap.tsx

--- components/RadarMap.tsx
+++ components/RadarMap.tsx
@@ -1,17 +1,46 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import "leaflet/dist/leaflet.css";
 import "@/styles/leaflet.css";
 import L from "leaflet";
-import { TileLayer } from "react-leaflet";
+import { TileLayer, Marker, Popup } from "react-leaflet";
 
 const MapContainer = dynamic(
-  async () => (await import("react-leaflet")).MapContainer,
+  () => import("react-leaflet").then(m => m.MapContainer),
   { ssr: false }
 );
 
+interface Aircraft {
+  callsign: string;
+  lat: number;
+  lon: number;
+  alt: number;
+  gs: number;
+  hdg: number;
+}
+
+// Purple unicorn icon 🎨🦄
+const planeIcon = L.icon({
+  iconUrl: "https://i.imgur.com/CUZ8eJW.png", // ✅ temp icon (we can replace with your custom one)
+  iconSize: [36, 36],
+  iconAnchor: [18, 18]
+});
+
 export default function RadarMap() {
+  const [planes, setPlanes] = useState<Record<string, Aircraft>>({});
+
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      const res = await fetch("/api/position");
+      const data = await res.json();
+      setPlanes(data);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="p-4">
       <MapContainer
@@ -21,6 +50,22 @@ export default function RadarMap() {
         style={{ background: "#000" }}
       >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+        {Object.values(planes).map((ac: Aircraft) => (
+          <Marker
+            key={ac.callsign}
+            position={[ac.lat, ac.lon] as L.LatLngExpression}
+            icon={planeIcon}
+            rotationAngle={ac.hdg}
+          >
+            <Popup>
+              <b>{ac.callsign}</b><br/>
+              Alt: {Math.round(ac.alt)} ft<br/>
+              GS: {Math.round(ac.gs)} kts<br/>
+              HDG: {Math.round(ac.hdg)}°
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
